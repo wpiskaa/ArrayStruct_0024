fb4e9aa697d6179ac27d53b6b4772695b09eebd4
wpiskaa <hafizzkurniawan19@gmail.com>
2025-04-14 11:02:47 +0700

    memberikan nilai array

--- Array.cpp
+++ Array.cpp
@@ -9,3 +9,31 @@ string nama[5]={"ben" , "agus" , "fara" , "ama" , "akila"};
 
 //mendeklarasikan array dengan ukuran dan tanpa nilai awal 
 float score[5];
+
+int main(){
+    cout << "Nama pada index 2 adalah " << nama[2] << endl;
+    //memberi nilai pada elemen array per index
+    nama[2]= "piska";
+    cout << "Nama pada index 2 adalah " << nama[2] << endl;
+
+    cout << "inputscore" << endl;
+    cout << endl;
+
+    //memberi nilai array dengan looping for
+    for(int i = 0; i<5; i++){
+        cout << "Score ke-" << i+1 << " =";
+        cin >> score[i];
+
+    }
+
+    cout << "Tampilkan skor" << endl;
+    cout << endl;
+    
+    //menampilkan nilai array dengan looping for
+    for(int i = 0; i<5; i++){
+        cout << "Score ke-" << i+1 << " =" <<score[i] << endl;
+        
+
+    }
+    
+}
