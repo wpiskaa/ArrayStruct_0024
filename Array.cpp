c9590cca9243a1a271eb4db407dd4a012442f270
wpiskaa <hafizzkurniawan19@gmail.com>
2025-04-14 10:44:35 +0700

    mendeklarasikan array

--- Array.cpp
+++ Array.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 using namespace std;
 
+//mendeklarasikan array tanpa ukuran, wajib diberi nilai awal
+int nilai[]={1,2,3};
+
+//mendeklarasikan array dengan ukuran dan diberi nilai awal
+string nama[5]={"ben" , "agus" , "fara" , "ama" , "akila"};
+
+//mendeklarasikan array dengan ukuran dan tanpa nilai awal 
+float score[5];
